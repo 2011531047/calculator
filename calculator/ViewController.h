c51667fc0806120cefce5809dc34d76e330c90a8
Nogami Hazuki <haapinn@gmail.com>
2013-12-05 15:40:09 +0900

    一桁同士の四則演算

--- calculator/ViewController.h
+++ calculator/ViewController.h
@@ -8,6 +8,30 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+    int nownumber, beforenumber;
+}
+
+- (IBAction)zero:(id)sender;
+- (IBAction)one:(id)sender;
+- (IBAction)two:(id)sender;
+- (IBAction)three:(id)sender;
+- (IBAction)four:(id)sender;
+- (IBAction)five:(id)sender;
+- (IBAction)six:(id)sender;
+- (IBAction)seven:(id)sender;
+- (IBAction)eight:(id)sender;
+- (IBAction)nine:(id)sender;
+
+- (IBAction)divisionButton:(id)sender;
+- (IBAction)multiplicationButton:(id)sender;
+- (IBAction)subtractionButton:(id)sender;
+- (IBAction)additionButton:(id)sender;
+- (IBAction)equal:(id)sender;
+- (IBAction)AC:(id)sender;
+@property enum {add, sub, mul, div} state;
+
+@property (weak, nonatomic) IBOutlet UILabel *result;
+
 
 @end
--- calculator/ViewController.m
+++ calculator/ViewController.m
@@ -24,4 +24,134 @@
     [super didReceiveMemoryWarning];
 }
 
+- (IBAction)zero:(id)sender {
+    nownumber = nownumber *10;
+    nownumber = nownumber +1;
+    _result.text = [NSString
+        stringWithFormat:@"%d",nownumber];
+}
+
+- (IBAction)one:(id)sender {
+    _result.text = @"1";
+}
+
+- (IBAction)two:(id)sender {
+    nownumber = nownumber *10;
+    nownumber = nownumber +2;
+    _result.text = [NSString
+        stringWithFormat:@"%d",nownumber];
+}
+
+- (IBAction)three:(id)sender {
+    nownumber = nownumber *10;
+    nownumber = nownumber +3;
+    _result.text = [NSString
+        stringWithFormat:@"%d",nownumber];
+}
+
+- (IBAction)four:(id)sender {
+    nownumber = nownumber *10;
+    nownumber = nownumber +4;
+    _result.text = [NSString
+        stringWithFormat:@"%d",nownumber];
+}
+
+- (IBAction)five:(id)sender {
+    nownumber = nownumber *10;
+    nownumber = nownumber +5;
+    _result.text = [NSString
+        stringWithFormat:@"%d",nownumber];
+}
+
+- (IBAction)six:(id)sender {
+    nownumber = nownumber *10;
+    nownumber = nownumber +6;
+    _result.text = [NSString
+        stringWithFormat:@"%d",nownumber];
+}
+
+- (IBAction)seven:(id)sender {
+    nownumber = nownumber *10;
+    nownumber = nownumber +7;
+    _result.text = [NSString
+        stringWithFormat:@"%d",nownumber];
+}
+
+- (IBAction)eight:(id)sender {
+    nownumber = nownumber *10;
+    nownumber = nownumber +8;
+    _result.text = [NSString
+        stringWithFormat:@"%d",nownumber];
+}
+
+- (IBAction)nine:(id)sender {
+    nownumber = nownumber *10;
+    nownumber = nownumber +9;
+    _result.text = [NSString
+        stringWithFormat:@"%d",nownumber];
+}
+
+- (IBAction)divisionButton:(id)sender {
+    beforenumber = nownumber;
+    nownumber = 0;
+    [self setState:div];
+    _result.text = [NSString stringWithFormat:@"%d", beforenumber];    
+}
+
+- (IBAction)multiplicationButton:(id)sender {
+    beforenumber = nownumber;
+    nownumber = 0;
+    [self setState:mul];
+    _result.text = [NSString stringWithFormat:@"%d", beforenumber];
+}
+
+- (IBAction)subtractionButton:(id)sender {
+    beforenumber = nownumber;
+    nownumber = 0;
+    [self setState:sub];
+    _result.text = [NSString stringWithFormat:@"%d", beforenumber];
+}
+
+- (IBAction)additionButton:(id)sender {
+    nownumber = beforenumber + nownumber;
+    beforenumber = nownumber;
+    nownumber = 0;
+    [self setState:add];
+    _result.text = [NSString stringWithFormat:@"%d", beforenumber];
+}
+
+- (IBAction)equal:(id)sender {
+    switch (self.state) {
+        case div:
+            nownumber = beforenumber / nownumber;
+            _result.text = [NSString stringWithFormat:@"%d", nownumber];
+            beforenumber = nownumber;
+            nownumber = 0;
+            break;
+        case mul:
+            nownumber = beforenumber * nownumber;
+            _result.text = [NSString stringWithFormat:@"%d", nownumber];
+            nownumber = 0;
+            break;
+        case sub:
+            nownumber = beforenumber - nownumber;
+            _result.text = [NSString stringWithFormat:@"%d", nownumber];
+            nownumber = 0;
+            break;
+        case add:
+            nownumber = beforenumber + nownumber;
+            _result.text = [NSString stringWithFormat:@"%d", nownumber];
+            beforenumber = nownumber;
+            nownumber = 0;
+            break;
+    }
+}
+
+- (IBAction)AC:(id)sender {
+    nownumber = 0;
+    beforenumber = 0;
+    _result.text = [NSString stringWithFormat:@"%d", nownumber];
+}
+
+
 @end
